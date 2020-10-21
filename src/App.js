c4f112255f6fd8f362e0aea96421d6b777598c8a
daxthenuke <55718715+daxthenuke@users.noreply.github.com>
2020-10-21 13:13:22 -0700

    srki radio exios

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import List from './components/List' ;
 import Add from './components/Add' ;
 import Update from './components/Update' ;
 
-import {Switch,Route , Link } from 'react-router-dom';
+import {BrowserRouter ,Route , NavLink } from 'react-router-dom';
 
 // List
 // Add form
@@ -15,32 +15,32 @@ import {Switch,Route , Link } from 'react-router-dom';
 function App() {
   return (
     <div className="container">
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-  <Link class="navbar-brand" to="/">Navbar</Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      
-        
-      <li class="nav-item">
-        <Link class="nav-link" to="/">List</Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/add">Add</Link>
-      </li>
-      
-    </ul>
-  </div>
-</nav>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+      <NavLink class="navbar-brand" to="/">Navbar</NavLink>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          
+            
+          <li class="nav-item">
+            <NavLink class="nav-link" to="/" exact={true}>List</NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink class="nav-link" to="/add" exact={true}>Add</NavLink>
+          </li>
+          
+        </ul>
+      </div>
+      </nav>
 
       
-      <Switch>
-        <Route path="/" component={List} exect />
-        <Route path="/add" component={Add} />
-        <Route path="/update/:id" component={Update} />
-      </Switch>
+      <BrowserRouter>
+        <Route path={'/'} exact={true} component={List} />
+        <Route path={'/add'}  exact={true} component={Add}/>
+        <Route path={"/update/:id"} exact={true} component={Update} />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,21 +1,34 @@
-import React from 'react' ;
+import React, { useState } from 'react' ;
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 
 function Add() {
+    const [formData, setFormData] = useState({fullName: '', email: ''})
+
+    const sendFromData = () => {
+        axios.post('/neki-url', formData)
+        .then(response => {
+
+        })
+        .catch(error => {
+
+        })
+    }
+
     return (
         < table className="table table-bordered">
         <tr>
             <th>Full Name</th>
             <td>
-                <input type="text" className= "form-control"/>
+                <input type="text" className= "form-control" value={formData.fullName} onChange={e => setFormData({...formData, fullName: e.target.value})}/>
             </td>
        
         </tr> 
         <tr>
             <th>Email</th>
             <td>
-                <input type="text" className= "form-control"/>
+                <input type="text" className= "form-control" value={formData.email} onChange={e => setFormData({...formData, email: e.target.value})} />
             </td>
         </tr>
         <tr>
@@ -35,7 +48,7 @@ function Add() {
         
         <tr>
             <td colSpan="2">
-                <input type ="submit" className="btn btn" />
+                <input onClick={sendFromData} type ="submit" className="btn btn" />
             </td>
         </tr>
 
