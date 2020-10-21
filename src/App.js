5207f5efa956415bc8fe94a30ede5c56c2efb9ba
daxthenuke <55718715+daxthenuke@users.noreply.github.com>
2020-10-21 07:52:40 -0700

    add link table wont open

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ import {Switch,Route , Link } from 'react-router-dom';
 function App() {
   return (
     <div className="container">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+  <Link class="navbar-brand" to="/">Navbar</Link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -25,19 +25,17 @@ function App() {
       
         
       <li class="nav-item">
-        <Link class="nav-link" to="#">List</Link>
+        <Link class="nav-link" to="/">List</Link>
       </li>
       <li class="nav-item">
-        <Link class="nav-link" to="#">Add</Link>
+        <Link class="nav-link" to="/add">Add</Link>
       </li>
       
     </ul>
   </div>
 </nav>
 
-      <Link to="/"> List</Link>
-      <Link to="/add">Add</Link>
-      <Link to="/update/1">Update</Link>
+      
       <Switch>
         <Route path="/" component={List} exect />
         <Route path="/add" component={Add} />
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,35 @@
 import React from 'react' ;
-
+import {Link} from 'react-router-dom';
 
 
 function List() {
     return (
-        <h3>List Employee</h3>
+       < table className="table table-bordered">
+        <thead>
+            <tr>
+                <th>Full Name</th>
+                <th>Email</th>
+                <th>Contact</th>
+                <th>Address</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+            <tr>
+                <td>John</td>
+                <td>john@gmail.com</td>
+                <td>74373743747347</td>
+                <td>JohnBeograd</td>
+                <td>
+                    <Link to="/update/1" className= "btn btn-info mr-3">Update</Link>
+                    <button className=" btn btn-danger">Delete</button>
+                </td>
+            </tr>
+        </tbody>
+        </table>
     );
+
 
 }
 
