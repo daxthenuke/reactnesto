3973d12ac5e88e1be92412c018e379112b127595
daxthenuke <55718715+daxthenuke@users.noreply.github.com>
2020-10-21 07:51:55 -0700

    add link not work

--- src/App.js
+++ src/App.js
@@ -15,6 +15,26 @@ import {Switch,Route , Link } from 'react-router-dom';
 function App() {
   return (
     <div className="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      
+        
+      <li class="nav-item">
+        <Link class="nav-link" to="#">List</Link>
+      </li>
+      <li class="nav-item">
+        <Link class="nav-link" to="#">Add</Link>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
+
       <Link to="/"> List</Link>
       <Link to="/add">Add</Link>
       <Link to="/update/1">Update</Link>
